02eb35f1185f5a1ed7a5adb6912dcbb68105ddae
Max Fu <max.fu.letian@gmail.com>
2020-09-08 22:13:15 -0700

    added bash files

---
+++ hw1/experiments/hopper-500.sh
@@ -0,0 +1,5 @@
+python cs285/scripts/run_hw1.py \
+        --expert_policy_file cs285/policies/experts/Hopper.pkl \
+        --env_name Hopper-v2 --exp_name bc_Hopper --n_iter 1 \
+        --expert_data cs285/expert_data/expert_data_Hopper-v2.pkl \
+        --video_log_freq -1 --ep_len 1000 --eval_batch_size 5000 --size 64 --num_agent_train_steps_per_iter 500 --batch_size 10000
