1a00adedd2a4176e341cb1fde59f9e540b3fcbbf
Max-Fu <max.fu.letian@gmail.com>
2020-10-08 10:56:39 -0700

    q5b script

---
+++ hw3/configs/q5b.sh
@@ -0,0 +1,1 @@
+python cs285/scripts/run_hw3_actor_critic.py --env_name HalfCheetah-v2 --ep_len 150 --discount 0.90 --scalar_log_freq 1 -n 150 -l 2 -s 32 -b 30000 -eb 1500 -lr 0.02 --exp_name q5_$1_$2 -ntu $1 -ngsptu $2
