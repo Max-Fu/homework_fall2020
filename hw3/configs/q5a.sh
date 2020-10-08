0f62abcc4cc6c0f1f4d147647146279401d4e64d
Max-Fu <max.fu.letian@gmail.com>
2020-10-08 14:08:08 -0700

    bug fix for question 2

--- hw3/configs/q5a.sh
+++ hw3/configs/q5a.sh
@@ -1,1 +1,1 @@
-python cs285/scripts/run_hw3_actor_critic.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 0.95 -n 100 -l 2 -s 64 -b 5000 -lr 0.01 --exp_name q5_$1_$2 -ntu $1 -ngsptu $2
+python cs285/scripts/run_hw3_actor_critic.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 0.95 -n 100 -l 2 -s 64 -b 5000 -lr 0.01 --seed 2 --exp_name q5_$1_$2 -ntu $1 -ngsptu $2
