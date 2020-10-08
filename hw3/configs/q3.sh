fc1d0e21a7431825d489bcab9e510fc1883ecd29
Max-Fu <max.fu.letian@gmail.com>
2020-10-08 11:03:20 -0700

    added question 3,4 scripts

--- hw3/configs/q3.sh
+++ hw3/configs/q3.sh
@@ -1,7 +1,5 @@
-python cs285/scripts/run_hw3_actor_critic.py --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_1_1 -ntu 1 -ngsptu 1
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_ep_len_200 --ep_len 200
 
-python cs285/scripts/run_hw3_actor_critic.py --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_100_1 -ntu 100 -ngsptu 1
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_ep_len_400 --ep_len 400
 
-python cs285/scripts/run_hw3_actor_critic.py --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_1_100 -ntu 1 -ngsptu 100
-
-python cs285/scripts/run_hw3_actor_critic.py --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_10_10 -ntu 10 -ngsptu 10
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_ep_len_800 --ep_len 800
